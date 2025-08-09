6a5c41010592ca4ffa01b5f5160c4f6d1b3970a6
h4969 <hemadl4969@gmail.com>
2025-08-09 22:10:24 +0530

    changed path to live in url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
 
-export const API_URL="http://localhost:4000"
+// export const API_URL="http://localhost:4000"
 
-// export const API_URL="https://backend-mern-stack-9tm9.onrender.com"
+export const API_URL="https://backend-mern-stack-9tm9.onrender.com"
